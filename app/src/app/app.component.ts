9aed0c74c8236b3f09376592d94d75fc2c9a7f18
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-10-25 10:23:02 +0200

    --wip-- [skip ci]

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Http} from '@angular/http';
+import * as _ from 'lodash';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  searchInput: string = '';
+  pokemon = null;
+  pokemonPicture = '';
+
+  public radarChartLabels:string[] = ['Speed', 'Defense', 'Attack', 'Hp', 'Special-Defense', 'Special-Attack'];
+  
+  public radarChartData:any = [
+     {data: [], label: 'Stats'}
+  ];
+
+  public radarChartType:string = 'radar';
+  
+  constructor(private http: HttpClient, private request: Http) {}
+
+  private clearPokemon() {
+    this.pokemon = null;
+    this.pokemonPicture = '';
+  }
+
+  searchPokemon() {
+    this.clearPokemon();
+
+    if (_.isEmpty(this.searchInput)) {
+      return;
+    }
+
+    this.request.get('http://localhost:3000/pokemon/'+this.searchInput).subscribe(data => {
+      console.log(data.json());
+
+      this.pokemon = data.json();
+      this.pokemonPicture = this.pokemon.sprites.front_default || this.pokemon.sprites.front_female;
+      this.radarChartData[0].data = [];
+      for (var i in this.radarChartLabels) {
+        let label = this.radarChartLabels[i];
+        let el = _.find(this.pokemon.stats, function(o) { return o.name === label.toLowerCase(); });
+        
+        console.log(el);
+        this.radarChartData[0].data.push(el.value);
+        console.log(this.radarChartData[0]);
+        
+     }
+
+    });
+  }
+
+  changeGender(type) {
+    if (this.pokemon === null) {
+      return;
+    }
+
+    this.pokemonPicture = this.pokemon.sprites['front_'+type];
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
